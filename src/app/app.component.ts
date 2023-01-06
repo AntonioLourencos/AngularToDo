a5be47254e0369acf32378714d6e9746baa5398e
AntonioLourencos <antonio.oficialcontato@gmail.com>
2023-01-06 15:21:31 -0300

    Simple ToDo with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { ToDo } from './to-do.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [],
 })
 export class AppComponent {
-  title = 'to-do';
+  toDo: ToDo[] = [];
+
+  onKeyDown(event: KeyboardEvent) {
+    if (
+      event.key.toLowerCase() != 'enter' ||
+      !(event.target as HTMLInputElement).value
+    )
+      return;
+
+    const newTodo = new ToDo();
+
+    newTodo.index = this.toDo.length;
+    newTodo.payload = (event.target as HTMLInputElement).value;
+    newTodo.checked = false;
+
+    this.toDo.push(newTodo);
+
+    (event.target as HTMLInputElement).value = '';
+  }
+
+  handleStatus(_index: number) {
+    this.toDo[_index].checked = !this.toDo[_index].checked;
+  }
+
+  handleDelete(_index: number) {
+    this.toDo.splice(_index, 1);
+  }
 }
---
+++ src/app/to-do.model.ts
@@ -0,0 +1,5 @@
+export class ToDo {
+  index: number;
+  payload: string;
+  checked: boolean;
+}
